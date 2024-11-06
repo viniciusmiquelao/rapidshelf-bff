4157a94f46b727d467538dcaa4c4772506912514
Vinicius Carvalho <viniciuscarvalho60@yahoo.com.br>
2024-11-05 21:37:36 -0300

    Fix Product Reponse DTO

--- src/main/java/com/rapidshelf/rapidshelf_bff/modules/products/dto/ProductResponseDTO.java
+++ src/main/java/com/rapidshelf/rapidshelf_bff/modules/products/dto/ProductResponseDTO.java
@@ -3,8 +3,8 @@ package com.rapidshelf.rapidshelf_bff.modules.products.dto;
 import com.rapidshelf.rapidshelf_bff.modules.products.entities.Product;
 import com.rapidshelf.rapidshelf_bff.modules.products.entities.ProductStatus;
 
-public record ProductResponseDTO(String id, String name, String description, Integer price, ProductStatus status) {
+public record ProductResponseDTO(String id, String name, String description, String userId, Integer price, ProductStatus status) {
     public ProductResponseDTO(Product product){
-        this(product.getId(), product.getName(),  product.getDescription(),  product.getPrice(),  product.getStatus());
+        this(product.getId(), product.getName(),  product.getDescription(), product.getUserId(),  product.getPrice(),  product.getStatus());
     }
 }
